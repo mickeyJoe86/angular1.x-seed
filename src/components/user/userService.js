d98dbdf858746d5b5096d684231532db3e05d6c2
Mike Ward <ward.michael51@gmail.com>
2017-06-30 16:42:26 -0400

    calls internal API

--- src/components/user/userService.js
+++ src/components/user/userService.js
@@ -1,17 +1,11 @@
 angular.module('components.userService', [])
     .service('UserService', ['$http', function ($http) {
-        var API = 'http://pokeapi.co/api/v2/pokemon/';
         var UserService = {};
-
-        UserService.findByName = function (name) {
-            return $http.get(API + name)
-                .then(function (res) {
+        UserService.getUsers = function () {
+            return $http.get('localhost:3000/name/')
+                .then(function(res) {
                     return res.data;
-                })
-                .catch(function (res) {
-                    return "Nope";
                 });
         };
-
         return UserService;
     }]);
--- tests/components/user/userService.spec.js
+++ tests/components/user/userService.spec.js
@@ -1,59 +1,74 @@
 describe('User service', () => {
     let UserService, $q, $httpBackend;
-    const API = 'http://pokeapi.co/api/v2/pokemon/';
-    const RESPONSE_SUCCESS = {
-        'id': 25,
-        'name': 'pikachu',
-        'sprites': {
-            'front_default': 'http://pokeapi.co/media/sprites/pokemon/25.png'
-        },
-        'types': [{
-            'type': { 'name': 'electric' }
-        }]
-    };
-    const RESPONSE_ERROR = {"detail": "Not found."};
+     
+    const USER_API = 'localhost:3000/name/'
+    const USER_SUCCESS = userList = [
+		{
+			id: '1',
+			name: 'Jane',
+			role: 'Designer',
+			location: 'New York',
+			twitter: 'gijane'
+		},
+		{
+			id: '2',
+			name: 'Bob',
+			role: 'Developer',
+			location: 'New York',
+			twitter: 'billybob'
+		},
+		{
+			id: '3',
+			name: 'Jim',
+			role: 'Developer',
+			location: 'Chicago',
+			twitter: 'jimbo'
+		},
+		{
+			id: '4',
+			name: 'Bill',
+			role: 'Designer',
+			location: 'LA',
+			twitter: 'dabill'
+		}
+	];
 
     beforeEach(angular.mock.module('components.userService'));
-
     beforeEach(inject((_UserService_, _$q_, _$httpBackend_) => {
         UserService = _UserService_;
         $q = _$q_;
         $httpBackend = _$httpBackend_;
     }));
 
-    // Verify our controller exists
     it('should exist', () => {
         expect(UserService).toBeDefined();
     });
 
-    describe('findByName()', () => {
+    describe('getUsers()', () => {
         let result;
         beforeEach(function () {
             result = {};
-            spyOn(UserService, "findByName").and.callThrough();
+            spyOn(UserService, 'getUsers').and.callThrough();
         });
         it('should be defined', () => {
-            expect(UserService.findByName).toBeDefined();
+            expect(UserService.getUsers).toBeDefined();
         });
-        it('should return a Pokemon when called with a valid name', () => {
-            let search = 'pikachu';            
-            $httpBackend.whenGET(API + search).respond(200, $q.when(RESPONSE_SUCCESS));
+        it('should return users', () => {
+            
+            $httpBackend.whenGET(USER_API).respond(200, $q.when(USER_SUCCESS));
 
-            expect(UserService.findByName).not.toHaveBeenCalled();
+            expect(UserService.getUsers).not.toHaveBeenCalled();
             expect(result).toEqual({});
 
-            UserService.findByName(search)
+            UserService.getUsers()
                 .then((res) => {
                     result = res;
                 });
-            
-            $httpBackend.flush();
 
-            expect(UserService.findByName).toHaveBeenCalledWith(search);
-            expect(result.id).toEqual(25);
-            expect(result.name).toEqual('pikachu');
-            expect(result.sprites.front_default).toContain('.png');
-            expect(result.types[0].type.name).toEqual('electric');
+            $httpBackend.flush();
+            
+            expect(result).toBeDefined();
+            expect(result).toEqual(USER_SUCCESS);
         });
     });
 });
